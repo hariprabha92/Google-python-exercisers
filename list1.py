956e65ab1a0d81e10610cc372a5c1640cdb11aaf
Hariprabha <hariprabha.haridas@gmail.com>
2016-07-31 23:11:45 +0530

    first commit

---
+++ list1.py
@@ -0,0 +1,64 @@
+def match_ends(words):
+  # +++your code here+++
+  count=0
+  for s in words:
+    l=len(s)
+    if l>=2 and s[0]==s[-1]:
+      count=count+1
+
+  return count
+def front_x(s):
+  p=[]
+  q=[]
+  for l in s:
+    if l.startswith('x')== True:
+      p.append(l)
+    else:
+      q.append(l)
+  return sorted(p) + sorted(q)
+def last(p):
+  return p[-1]
+
+def sort_last(s):
+  
+  a=sorted(s,key=last)
+  return a
+  
+
+def test(got, expected):
+  if got == expected:
+    prefix = ' OK '
+  else:
+    prefix = '  X '
+  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+
+
+# Calls the above functions with interesting inputs.
+def main():
+  print 'match_ends'
+  test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
+  test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
+  test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
+
+
+  print 'front_x'
+  test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
+       ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
+  test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
+       ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
+  test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
+       ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
+
+  print 'sort_last'
+  test(sort_last([(1, 3), (3, 2), (2, 1)]),
+       [(2, 1), (3, 2), (1, 3)])
+  test(sort_last([(2, 3), (1, 2), (3, 1)]),
+       [(3, 1), (1, 2), (2, 3)])
+  test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
+       [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
+
+
+
+if __name__ == '__main__':
+  main()
+
